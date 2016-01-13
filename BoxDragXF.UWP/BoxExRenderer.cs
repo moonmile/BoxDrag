a85123774afd8c5b189fb0b86674dc72250d7ded
Tomoaki Masuda <masuda@moonmile.net>
2016-01-13 16:32:40 +0900

    UWP 版を追加

---
+++ BoxDragXF.UWP/BoxExRenderer.cs
@@ -0,0 +1,30 @@
+﻿using BoxDragXF;
+using BoxDragXF.WinPhone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(BoxViewEx), typeof(BoxExRenderer))]
+namespace BoxDragXF.WinPhone
+{
+    class BoxExRenderer : Xamarin.Forms.Platform.UWP.BoxViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+            this.ManipulationDelta += BoxExRenderer_ManipulationDelta1;
+            this.ManipulationMode = Windows.UI.Xaml.Input.ManipulationModes.All;
+        }
+
+        private void BoxExRenderer_ManipulationDelta1(object sender, Windows.UI.Xaml.Input.ManipulationDeltaRoutedEventArgs e)
+        {
+            var el = this.Element as BoxViewEx;
+            el.OnManipulationDelta(el, new BoxDragXF.ManipulationDeltaRoutedEventArgs(el, e.Delta.Translation.X, e.Delta.Translation.Y));
+        }
+    }
+
+}
